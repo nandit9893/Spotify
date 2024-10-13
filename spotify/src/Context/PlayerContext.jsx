3d9246c70f3f46edca61b68e20b7747a50eae327
NANDIT SHARMA <137511995+nandit9893@users.noreply.github.com>
2024-10-13 22:28:01 +0530

    Update PlayerContext.jsx

--- spotify/src/Context/PlayerContext.jsx
+++ spotify/src/Context/PlayerContext.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 export const PlayerContext = createContext();
 
 const PlayerContextProvider = ({ children }) => {
-  const url = "https://spotify-back-end.onrender.com/";
+  const url = "https://spotify-back-end.onrender.com";
   const audioRef = useRef();
   const seekBg = useRef();
   const seekBar = useRef();
